bae989825072e775fc1fb8495b98b04036b3534e
Tweunis <147374955+Tweunis@users.noreply.github.com>
2023-10-23 02:46:59 +0200

    Add files via upload

--- Solar Toque Complete Orbit.py
+++ Solar Toque Complete Orbit.py
@@ -1,6 +1,6 @@
 import numpy as np
 import math as m
-from sympy import sin, cos, tan, pi
+from sympy import sin, cos, pi
 import matplotlib.pyplot as plt
 
 #%%
@@ -8,13 +8,14 @@ CR = 2
 GscoverR2 = 590 # /m^2
 c = 3*10**6 # m/s
 
-dtheta = np.pi/50
-theta = np.arange(0, 2*np.pi + dtheta, dtheta) # Angle in orbit
+dtheta = np.pi/1000
+theta = np.arange(0, 2*np.pi, dtheta) # Angle in orbit
 
 A_bus = 2.045 # m^2
 r_b = 0.480 # m
 r_a = 1.04938 # m
 h = 1.050 # m
+L_bus = 2.13 # m
 
 def cross_prod(a, b):
     result = [a[1]*b[2] - a[2]*b[1],
@@ -23,100 +24,118 @@ def cross_prod(a, b):
 
     return result
 
+def add(a, b):
+    result = [a[0]+b[0],
+            a[1]+b[1],
+            a[2]+b[2]]
+
+    return result
+
 #%% Rotating Force unit vector 
 
 n_F2 = []
+            
 for i in theta:
-    if  0 < i < pi/2:
-        for i in theta:
-            n_F2.append([sin(i), 0, cos(i)])
-    elif pi/2 < i < pi:
-        for i in theta:
-            n_F2.append([sin(i), 0, -cos(i)])
-    elif pi < i < 3/2*pi:
-        for i in theta:
-            n_F2.append([sin(i), 0, -cos(i)])
-    elif 3/2*pi < i < 2*pi:
-        for i in theta:
-            n_F2.append([sin(i), 0, cos(i)])
-
+    if  0 <= i < pi/2:
+        n_F2.append([sin(i), 0, -cos(i)])
+    elif pi/2 <= i < pi:
+        n_F2.append([sin(i), 0, cos(i)])
+    elif pi <= i < 3/2*pi:
+        n_F2.append([sin(i), 0, cos(i)])
+    elif 3/2*pi <= i <= 2*pi:
+        n_F2.append([sin(i), 0, -cos(i)])
+            
 n_F = np.array(n_F2)
 
 
 # %% Bus
-phiQ2 = []
-phiQ3 = []
-phiQ4 = []
-for i in theta:
-    phiQ2.append(np.pi-i)
-    phiQ3.append(i-np.pi)
-    phiQ4.append(2*np.pi-i)
-            
+
 r_bus = np.array([0.028367, -0.015365, -0.232788])
 
 A_bus2 = []
 r_bus2 = []
 for i in theta:
-    if 0 < i < pi/2 or pi < i < 3/2*pi:
-        for i in theta:
-            y = r_a - r_b/cos(i) - h*sin(i) + r_b*tan(i)*sin(i)
-            x = y / sin(i)
-            if A_bus*sin(i) - 2*r_b* y >= 0:
+    if 0 <= i < pi/2:
+            i = -i
+            y = sin(i)**2*(r_a-r_b/cos(i))+sin(i)**2*r_b/cos(i)-h*sin(i)
+            x = sin(i)*(r_a-r_b/cos(i))+sin(i)*r_b/cos(i)-h
+            if A_bus*sin(i) > 2*r_b*y:
                 A_bus2.append(A_bus*sin(i) - 2*r_b* y)
                 x_COA = -x/2 # delta z in catia
-                deltaRbus = np.array([0, 0, x_COA])
-                r_bus2.append(np.add(r_bus, deltaRbus))
+                if pi/2 - pi/100 < i < pi/2 + pi/100:
+                    x_COA = 0
+                if x_COA <= L_bus:
+                    deltaRbus = np.array([0, 0, x_COA])
+                    r_bus2.append(add(r_bus, deltaRbus))
+                else:
+                    r_bus2.append(r_bus)
             else:
                 A_bus2.append(0)
                 r_bus2.append(r_bus)
         
     elif pi/2 < i < pi:
-        for i in phiQ2:
-            y = r_a - r_b/cos(i) - h*sin(i) + r_b*tan(i)*sin(i)
-            x = y / sin(i)
-            if A_bus*sin(i) - 2*r_b* y >= 0:
+            i = i-np.pi
+            y = sin(i)**2*(r_a-r_b/cos(i))+sin(i)**2*r_b/cos(i)-h*sin(i)
+            x = sin(i)*(r_a-r_b/cos(i))+sin(i)*r_b/cos(i)-h
+            if A_bus*sin(i) > 2*r_b*y:
                 A_bus2.append(A_bus*sin(i) - 2*r_b* y)
                 x_COA = -x/2 # delta z in catia
-                deltaRbus = np.array([0, 0, x_COA])
-                r_bus2.append(np.add(r_bus, deltaRbus))
+                if pi/2 - pi/100 < i < pi/2 + pi/100:
+                    x_COA = 0
+                if x_COA <= L_bus:
+                    deltaRbus = np.array([0, 0, x_COA])
+                    r_bus2.append(add(r_bus, deltaRbus))
+                else:
+                    r_bus2.append(r_bus)
             else:
                 A_bus2.append(0)
                 r_bus2.append(r_bus)
         
     elif pi < i < 3/2*pi:
-        for i in phiQ3:
-            y = r_a - r_b/cos(i) - h*sin(i) + r_b*tan(i)*sin(i)
-            x = y / sin(i)
-            if A_bus*sin(i) - 2*r_b* y >= 0:
+            i = np.pi-i
+            y = sin(i)**2*(r_a-r_b/cos(i))+sin(i)**2*r_b/cos(i)-h*sin(i)
+            x = sin(i)*(r_a-r_b/cos(i))+sin(i)*r_b/cos(i)-h
+            if A_bus*sin(i) > 2*r_b*y:
                 A_bus2.append(A_bus*sin(i) - 2*r_b* y)
                 x_COA = -x/2 # delta z in catia
-                deltaRbus = np.array([0, 0, x_COA])
-                r_bus2.append(np.add(r_bus, deltaRbus))
+                if pi/2 - pi/100 < i < pi/2 + pi/100:
+                    x_COA = 0
+                if x_COA <= L_bus:
+                    deltaRbus = np.array([0, 0, x_COA])
+                    r_bus2.append(add(r_bus, deltaRbus))
+                else:
+                    r_bus2.append(r_bus)
             else:
                 A_bus2.append(0)
                 r_bus2.append(r_bus)
         
-    elif 3/2*pi < i < 2*pi:
-        for i in phiQ4:
-            y = r_a - r_b/cos(i) - h*sin(i) + r_b*tan(i)*sin(i)
-            x = y / sin(i)
-            if A_bus*sin(i) - 2*r_b* y >= 0:
+    elif 3/2*pi < i <= 2*pi:
+            i = i-2*np.pi
+            y = sin(i)**2*(r_a-r_b/cos(i))+sin(i)**2*r_b/cos(i)-h*sin(i)
+            x = sin(i)*(r_a-r_b/cos(i))+sin(i)*r_b/cos(i)-h
+            if A_bus*sin(i) > 2*r_b*y:
                 A_bus2.append(A_bus*sin(i) - 2*r_b* y)
                 x_COA = -x/2 # delta z in catia
-                deltaRbus = np.array([0, 0, x_COA])
-                r_bus2.append(np.add(r_bus, deltaRbus))
+                if pi/2 - pi/100 < i < pi/2 + pi/100:
+                    x_COA = 0
+                if x_COA <= L_bus:
+                    deltaRbus = np.array([0, 0, x_COA])
+                    r_bus2.append(add(r_bus, deltaRbus))
+                else:
+                    r_bus2.append(r_bus)
             else:
                 A_bus2.append(0)
                 r_bus2.append(r_bus)
+
 
 F_bus = []
 for i in range(len(theta)):
     F_bus.append(n_F[i] * CR * GscoverR2 / c * A_bus2[i])
 
-
 tau_bus = []
 for i in range(len(theta)):
     tau_bus.append(cross_prod(r_bus2[i], F_bus[i]))
+
 
 #%% Antenna
 
@@ -152,12 +171,6 @@ for i in range(len(theta)):
 
 #%% Total torque per axis
 
-# THIS IS PART OF A VERY BIG COMMENT
-# THIS IS PART OF A VERY BIG COMMENT
-# THIS IS PART OF A VERY BIG COMMENT
-# THIS IS PART OF A VERY BIG COMMENT
-# THIS IS PART OF A VERY BIG COMMENT
-
 tau_total_x = []
 tau_total_y = []
 tau_total_z = []
@@ -165,26 +178,18 @@ for i in range(len(theta)):
     tau_total_x.append((tau_array1[i][0] + tau_array2[i][0] + tau_bus[i][0] + tau_ant[i][0])*1000)
     tau_total_y.append((tau_array1[i][1] + tau_array2[i][1] + tau_bus[i][1] + tau_ant[i][1])*1000)
     tau_total_z.append((tau_array1[i][2] + tau_array2[i][2] + tau_bus[i][2] + tau_ant[i][2])*1000)
-    
-# THIS IS PART OF A VERY BIG COMMENT
-# THIS IS PART OF A VERY BIG COMMENT
-# THIS IS PART OF A VERY BIG COMMENT
-# THIS IS PART OF A VERY BIG COMMENT
-# THIS IS PART OF A VERY BIG COMMENT
-# THIS IS PART OF A VERY BIG COMMENT
-    
 
 #%% Plotting
 
 fig, (ax1, ax2) = plt.subplots(1, 2)
-ax1.plot(theta, tau_total_x, c="r", linestyle = "dashed", label="$𝜏_x$")
-ax1.plot(theta, tau_total_z, c="b", linestyle = "dashed", label="$𝜏_z$")
-ax2.plot(theta, tau_total_y, c="g", linestyle = "dashed", label="$𝜏_y$")
+ax1.plot(theta, tau_total_x, c="r",  label="$T_x$")
+ax1.plot(theta, tau_total_z, c="b",  label="$T_z$")
+ax2.plot(theta, tau_total_y, c="g",  label="$T_y$")
 
 ax1.set_xlabel('True anomaly angle $θ$ [rad]')
-ax1.set_ylabel('Torque $𝜏$ [mN$\cdot$m]')
+ax1.set_ylabel('Solar Radiation Torque $T_s$ [mN$\cdot$m]')
 ax2.set_xlabel('True anomaly angle $θ$ [rad]')
-ax2.set_ylabel('Torque $𝜏$ [mN$\cdot$m]')
+ax2.set_ylabel('Solar Radiation Torque $T_s$ [mN$\cdot$m]')
 ax1.legend(loc='lower left')
 ax2.legend(loc='lower left')
 
@@ -202,7 +207,7 @@ ax2.grid()
 plt.show()
 plt.figure(dpi = 1200)
 
-#fig.savefig('Torque_Plots.pdf')
+fig.savefig('Torque_Plots.pdf')
 
 
 
